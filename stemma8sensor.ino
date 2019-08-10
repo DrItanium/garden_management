845f210a1e5c9df83b213fc6d0f615c63ae13322
Joshua Scoggins <Theoretically.x64@gmail.com>
2019-08-10 11:28:55 -0700

    Don't be a dick, zlib license all the way

--- stemma8sensor.ino
+++ stemma8sensor.ino
@@ -1,3 +1,31 @@
+/**
+ * @file 
+ * hook up an i2c expander to 8 stemma soil sensors and a OLED display
+ * basis of this code is the SSD1306 OLED display example. Code has been 
+ * adapted to be a little more robust in case of accidental disconnect.
+ * 
+ * This is being used in my garden to track the states of 8 different plants
+ * (mostly peppers). I wish the stemma sensor returned values greater than 1016
+ * but that seems to be a limitation of the seesaw library.
+ * @copyright
+ * Copyright (c) 2019 Joshua Scoggins 
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would be
+ *    appreciated but is not required.
+ * 2. Altered source versions must be plainly marked as such, and must not be
+ *    misrepresented as being the original software.
+ * 3. This notice may not be removed or altered from any source distribution.
+ */
 /*
  * garden_management
  * Copyright (c) 2019, Joshua Scoggins
@@ -22,13 +50,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-// hook up an i2c expander to 8 stemma soil sensors and a OLED display
-// basis of this code is the SSD1306 OLED display example. Code has been 
-// adapted to be a little more robust in case of accidental disconnect.
-
-// This is being used in my garden to track the states of 8 different plants
-// (mostly peppers). I wish the stemma sensor returned values greater than 1016
-// but that seems to be a limitation of the seesaw library.
 
 #include <SPI.h>
 #include <Wire.h>
